24d26bf840f38c2489571788611302978a6b40e6
César Montero <72035439+cesarlmt27@users.noreply.github.com>
2023-11-05 21:06:07 -0300

    Archivo movido y renombrado

--- spark/driver-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# https://spark.apache.org/docs/latest/spark-standalone.html
-
-exec ./sbin/start-connect-server.sh --packages org.apache.spark:spark-connect_2.12:3.5.0 &
-
-exec ./sbin/start-master.sh &
-
-sleep infinity
